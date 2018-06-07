266ee9c874d6bc9f51692b5a0bbf5e82107dd21d
n-del <nbahmanyar@zoho.com>
2018-06-07 16:22:16 -0400

    cat clicker update

--- js/app.js
+++ js/app.js
@@ -1,14 +1,58 @@
-var ViewModel = function () {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tab');
-    this.imgSrc = ko.observable('img/1413379559_412a540d29_z.jpg');
-    this.imgAttribution = ko.observable('foo');
+var initialCats = [{
+        clickCount: 0,
+        name: 'Tabby',
+        imgSrc: 'img/22252709_010df3379e_z.jpg',
+        imgAttribution: 'foo',
+        nicknames: ['abc', 'fo']
+    },
+    {
+        clickCount: 0,
+        name: 'Tiger',
+        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution: 'foo',
+        nicknames: ['bar', 'foo']
+    }
+];
 
-    this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
-    };
-    console.log('foobar');
+var Cat = function (data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    this.nicknames = ko.observableArray(data.nicknames);
+    this.level = ko.computed(function () {
+        var level = 'n/a';
+        if (this.clickCount() < 10) {
+            level = 'Infant';
+        } else if (this.clickCount() < 20) {
+            level = 'Child';
+        } else if (this.clickCount() < 30) {
+            level = 'Teen';
+        } else {
+            level = 'Adult';
+        }
+        return level;
+    }, this);
 }
 
+// Define the view model
+var ViewModel = function () {
+    var self = this;
+
+    this.catList = ko.observableArray([])
+
+    initialCats.forEach(function (catItem) {
+        self.catList.push(new Cat(catItem));
+    });
+
+    this.currentCat = ko.observable(this.catList()[0])
+    this.incrementCounter = function () {
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+    };
+    this.setCat = function (clickedCat) {
+        self.currentCat(clickedCat);
+    }
+}
+
+
 ko.applyBindings(new ViewModel());
-console.log('foo initialized');
