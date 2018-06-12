91dabe7a2ea8edbce0d00570fbc1555f8d15832d
navid <nbahmanyar@zoho.com>
2018-06-11 22:01:59 -0400

    js google maps api

--- ud989-todo-app-master/js/views/todo-view.js
+++ ud989-todo-app-master/js/views/todo-view.js
@@ -21,6 +21,7 @@ var app = app || {};
 			'dblclick label': 'edit',
 			'click .destroy': 'clear',
 			'click .edit-btn': 'edit',
+			'click .priority-btn': 'togglePriority',
 			'keypress .edit': 'updateOnEnter',
 			'keydown .edit': 'revertOnEscape',
 			'blur .edit': 'close'
@@ -51,6 +52,7 @@ var app = app || {};
 
 			this.$el.html(this.template(this.model.toJSON()));
 			this.$el.toggleClass('completed', this.model.get('completed'));
+			this.$el.toggleClass('priority', this.model.get('priority'));
 			this.toggleVisible();
 			this.$input = this.$('.edit');
 			return this;
@@ -59,6 +61,11 @@ var app = app || {};
 		toggleVisible: function () {
 			this.$el.toggleClass('hidden', this.isHidden());
 		},
+
+		// togglePriority: function () {
+		// 	// this.$el.toggleClass('priority', this.isHidden());
+		// 	this.$el.toggleClass('priority');
+		// },
 
 		isHidden: function () {
 			return this.model.get('completed') ?
@@ -69,6 +76,11 @@ var app = app || {};
 		// Toggle the `"completed"` state of the model.
 		toggleCompleted: function () {
 			this.model.toggle();
+		},
+
+		// Toggle the `"priority"` state of the model.
+		togglePriority: function () {
+			this.model.togglePriority();
 		},
 
 		// Switch this view into `"editing"` mode, displaying the input field.
