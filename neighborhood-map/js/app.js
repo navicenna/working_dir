7c7cb5d2bc5decb4dc6d07ee154f5f99873e9fb2
navid <nbahmanyar@zoho.com>
2018-06-19 07:38:13 -0400

    side bar update

--- neighborhood-map/js/app.js
+++ neighborhood-map/js/app.js
@@ -1,3 +1,13 @@
+// Initialize a list of places in Austin, TX.
+  var locations = ko.observableArray([
+    {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+    {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+    {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+    {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+    {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+    {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+  ]);
+
 var map;
 var markers = [];
 
@@ -9,16 +19,6 @@ function initMap() {
       mapTypeControl: false
     });
 
-    // These are the real estate listings that will be shown to the user.
-    // Normally we'd have these in a database instead.
-    var locations = [
-      {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-      {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-      {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-      {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-      {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-      {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
-    ];
 
     var largeInfowindow = new google.maps.InfoWindow();
 
